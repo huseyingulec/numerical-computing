cfcc3a4867fbe002a1c1812679e194fc7e91dab5
Huseyin Gulec <itshuseyingulec@gmail.com>
2025-04-18 04:22:30 +0200

    add build files

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,13 @@
 import nextra from "nextra";
 
 const withNextra = nextra({
-    search: true,
-    defaultShowCopyCode: true,
-    
+  search: true,
+  defaultShowCopyCode: true,
 });
 
 export default withNextra({
-    // ... Other Next.js config options
-    // output: 'export'
+  output: "export",
+
+  // ... Other Next.js config options
+  // output: 'export'
 });
---
+++ out/_next/static/chunks/pages/_app-fac432ede7a90c65.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{2378:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(8768)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[593,792],()=>(n(2378),n(7918))),_N_E=_.O()}]);
